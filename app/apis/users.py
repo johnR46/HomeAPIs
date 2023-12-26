7bf204ac1a948889b752b1c141d530f01ae0affa
JohnR46 <machine.The@hotmail.com>
2023-12-26 16:20:38 +0700

    init

---
+++ app/apis/users.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+from fastapi import APIRouter
+
+router = APIRouter(
+    prefix="/users",
+    tags=["Users"],
+    responses={404: {"message": "Not found"}}
+)
+
+users_obj = [
+    {'id': 1, 'name': 'john'},
+    {'id': 2, 'name': 'jame'},
+    {'id': 3, 'name': 'jj'}
+]
+
+
+@router.get("/")
+def users():
+    return users_obj
+
+
+@router.get("/users/{user_id}")
+def read_item(user_id: int):
+    result = [u for u in users_obj if u.get('id') == user_id]
+    return result
+
+
+@router.get("/abc")
+def read_users():
+    return {"message": "abc"}
